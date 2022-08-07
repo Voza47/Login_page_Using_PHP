fdbf328714a522ade44bccb27b0d2c03153a9ed0
Voza47 <109664716+Voza47@users.noreply.github.com>
2022-08-07 15:36:13 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,42 @@
+<?php
+include ' C:\xampp\htdocs\Viraj\partials\_dbconnect.php';
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Signup page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php require 'partials/_nav.php'?>
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> Your account is now created and you can login
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+    <div class="container">
+        <h1 class="text-cneter">SignUp to your website</h1>
+    </div>
+    <form action="/Viraj/Signup.php">
+  <div class="form-group col-md-6">
+    <label for="username"  class="form-label">Username</label>
+    <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+    <small id="emailHelp" class="form-text text-muted">Make sure to type the same password</small>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+
+  </body>
+</html>
